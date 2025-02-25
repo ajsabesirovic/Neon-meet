957c532694aad98158c0ce0bcf27747d03a6f94d
Ajsa <besirovicajsa@gmail.com>
2025-02-25 15:43:09 +0100

    Queries and mutations for interviews and comments

--- convex/schema.ts
+++ convex/schema.ts
@@ -8,6 +8,26 @@ export default defineSchema({
     role: v.union(v.literal("candidate"), v.literal("interviewer")), // candidate or interviewer
     clerkId: v.string(),
   }).index("by_clerk_id", ["clerkId"]),
+
+  interviews: defineTable({
+    title: v.string(),
+    description: v.optional(v.string()),
+    startTime: v.number(),
+    endTime: v.optional(v.number()),
+    status: v.string(),
+    streamCallId: v.string(),
+    candidateId: v.string(),
+    interviewersId: v.array(v.string()),
+  })
+    .index("by_candidate_id", ["candidateId"])
+    .index("by_stream_call_id", ["streamCallId"]),
+
+  comments: defineTable({
+    content: v.string(),
+    rating: v.number(),
+    interviewerId: v.string(),
+    interviewId: v.id("interviews"),
+  }).index("by_interview_id", ["interviewId"]),
 });
 
 // https://trusty-toad-46.clerk.accounts.dev
