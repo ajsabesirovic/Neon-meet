0c00bf215cdf3881f4acee85f73ad11fd92bd5ce
Ajsa <besirovicajsa@gmail.com>
2025-03-02 17:30:28 +0100

    Set up the stream provider

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,7 @@
+import StreamClientProvider from "@/components/providers/StreamClientProvider";
+import { ReactNode } from "react";
+const Layout = ({ children }: { children: ReactNode }) => {
+  return <StreamClientProvider>{children}</StreamClientProvider>;
+};
+
+export default Layout;
