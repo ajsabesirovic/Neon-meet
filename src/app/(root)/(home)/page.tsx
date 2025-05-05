c92a980cc7dfeed08e9ca88c4711cb8626f61d63
Ajsa <besirovicajsa@gmail.com>
2025-05-05 15:40:06 +0200

    Implemented interview scheduling logic with Stream call creation and Convex mutation

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -7,6 +7,7 @@ import { api } from "../../../../convex/_generated/api";
 import { useQuery } from "convex/react";
 import { useRouter } from "next/navigation";
 import MeetingModal from "@/components/MeetingModal";
+import LoaderUI from "@/components/LoaderUI";
 
 export default function Home() {
   const router = useRouter();
@@ -32,7 +33,7 @@ export default function Home() {
     }
   };
 
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading) return <LoaderUI />;
   return (
     <div className="container max-w-7xl mx-auto p-6">
       <div className="rounded-lg bg-card p-6 border shadow-sm mb-10">
